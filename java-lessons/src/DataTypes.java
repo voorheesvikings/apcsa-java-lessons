25e9d0da95574fd53c5f329b5da65ef5a3d1195e
voorheesvikings <ramji20273736@student.nhvweb.net>
2025-12-03 13:28:51 +0000

    feat: dataTypes class creation

---
+++ java-lessons/src/DataTypes.java
@@ -0,0 +1,21 @@
+
+public class DataTypes {
+    public static void main(String[] args) {
+
+        DataTypeExamples dt = new DataTypeExamples();
+        System.out.println(dt.c);
+    }
+}
+
+// *Java is strongly typed,
+// ! You must define variable with datatype
+
+class DataTypeExamples {
+    int i = 10;
+    double  d = 20.1;
+    char c = 'h';
+    boolean b = true;
+
+
+    String s = "Hello World";
+}
