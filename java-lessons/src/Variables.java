f32330cc05b8ea5a8bce12966707aa924fa26b66
voorheesvikings <ramji20273736@student.nhvweb.net>
2025-12-04 14:19:37 +0000

    feat: variables done

---
+++ java-lessons/src/Variables.java
@@ -0,0 +1,28 @@
+// * Two types of variables: instance variables and static variables
+// * Variables are camelCase
+// * constants use UPPER_SNAKE_CASE
+
+class Student {
+    String studentName; // * Instance variable
+    static int studentCount = 0; // * Static variable: only belongs to the class
+    static final String SCHOOL_NAME = "CSAEA"; // * Constant - Never changes
+
+    Student(String studentName) {
+        this.studentName = studentName; //belongs to the specific object
+        studentCount++;
+        System.out.println(this.studentName + " is at " + SCHOOL_NAME + " for a total of " + studentCount + " students");
+        int counter = 0;
+        
+    }
+
+}
+
+public class Variables {
+    public static void main(String[] args) {
+        Student goat = new Student("goat");
+        Student corbin = new Student("corbin");
+        Student haha = new Student("haha");
+        Student moo = new Student("mooo");
+
+    }
+}
