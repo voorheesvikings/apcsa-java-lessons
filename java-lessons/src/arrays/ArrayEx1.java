7a04e09e43961c1f6576eba97896f05893890883
voorheesvikings <ramji20273736@student.nhvweb.net>
2026-01-07 14:27:57 +0000

    arraylesson

---
+++ java-lessons/src/arrays/ArrayEx1.java
@@ -0,0 +1,29 @@
+package arrays;
+public class ArrayEx1 {
+
+    public static void main(String args[]) {
+        
+        long[] primeNum = new long[20];
+        int[] even= new int[5];
+
+
+        long[] primes = new long[20];
+        primes[0] = 2;
+        primes[1] = 3;
+        long[] primes2 = primes;
+        System.out.println(primes2[0]);
+        primes2[0] = 5;
+        System.out.println(primes[0]);
+
+        // exercise 2
+        int[] score = new int[9];
+        byte[] values = new byte[10];
+
+        for (int i = 0; i < values.length; i++) {
+            values[i] = (byte)(1);
+            System.out.println(values[i]);
+        }
+
+    }
+
+}
